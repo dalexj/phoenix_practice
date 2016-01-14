cd7b191e35e4061b53813d4e254ab62956cf5ea0
Alex Jensen <96dalex@gmail.com>
2016-01-14 10:14:46 -0700

    missed a paren

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :hello, Hello.Endpoint,
   url: [scheme: "https", host: "radiant-chamber-3732.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # for heroku
 config :hello, Hello.Repo,
